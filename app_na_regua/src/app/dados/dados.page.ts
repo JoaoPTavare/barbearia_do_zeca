929d94e018b8f467aaecd7cf7814c693243f3e72
JoaoPTavare <jjoaopedrotava05@gmail.com>
2023-04-19 20:25:32 -0300

    iniciando criando a page e service

---
+++ app_na_regua/src/app/dados/dados.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-dados',
+  templateUrl: './dados.page.html',
+  styleUrls: ['./dados.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class DadosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
